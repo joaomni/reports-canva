35a9f801d51d377cdbcfdbc7fb0c33fc5ea61f10
João Paulo <joaojpmoreira25@gmail.com>
2023-07-01 17:35:54 +0000

    feat: report fvm

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,12 @@ import moment from 'moment'
 
 import './App.css'
 
-import CMF from "./pdf/30 meta_cmf.pdf"
-import Frizon from "./pdf/30 google_bellafrizon.pdf"
-import Pizzini  from "./pdf/30 google_clinica.pdf"
-import BBJ from "./pdf/30 meta_google_casamentos.pdf"
-import Hipica from "./pdf/30 meta_whats.pdf"
+import CMF from "./pdf/01 meta_cmf.pdf"
+import FVM from "./pdf/01 meta_fvm.pdf"
+import Frizon from "./pdf/01 google_bellafrizon.pdf"
+import Pizzini  from "./pdf/01 google_clinica.pdf"
+import BBJ from "./pdf/01 meta_google_casamentos.pdf"
+import Hipica from "./pdf/01 meta_whats.pdf"
 
 import Report from './components/Report'
 import Backtop from './components/Backtop'
@@ -20,11 +21,12 @@ function App() {
     <>
       <nav id='navbar'>
         <ul>
+          <li><a href="#fvm">FVM</a></li>
           <li><a href="#cmf">CMF</a></li>
           <li><a href="#hipica">Hípica</a></li>
           <li><a href="#bbje">BBJ Eventos</a></li>
-          <li><a href="#frizon">Frizon Contrutora</a></li>
           <li><a href="#pizzini">Dr. Fábio Pizzini</a></li>
+          <li><a href="#frizon">Frizon Contrutora</a></li>
           <li><a href="#orcamentos">Orçamentos</a></li>
         </ul>
       </nav>
@@ -32,9 +34,10 @@ function App() {
       <div className="container">
 
         <div className='title'>
-          <h2>Relatórios de {date}</h2>
+          <h2>Relatórios diário {date}</h2>
         </div>
         
+        <Report id="fvm" PDF={FVM}  Drive="https://drive.google.com/drive/u/2/folders/1SDT0fidAJKmx9qKO4jlDoN_mnLmBSWuo" Name="César Menotti e Fabiano"/>
         <Report id="cmf" PDF={CMF}  Drive="https://drive.google.com/drive/u/2/folders/1SDT0fidAJKmx9qKO4jlDoN_mnLmBSWuo" Name="César Menotti e Fabiano"/>
         <Report id="frizon" PDF={Frizon}  Drive="https://drive.google.com/drive/u/2/folders/1RsFP3cPFl95C7JpIgmPwzdfKXj-9WnND" Name="Frizon"/>
         <Report id="pizzini" PDF={Pizzini}  Drive="https://drive.google.com/drive/u/2/folders/1HUivfJzVGyQg7mXN07Tghn3HjU5wIC4Y" Name="Dr. Fábio Pizzini"/>
